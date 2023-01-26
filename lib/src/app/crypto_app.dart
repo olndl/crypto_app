4ca0943f0fa07b41441e6ae787be4d684d22d657
Olga Shiryavceva <olundel@yandex-team.ru>
2023-01-26 20:35:57 +0300

    build(app): add architecture

---
+++ lib/src/app/crypto_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class CryptoApp extends StatelessWidget {
+  const CryptoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
